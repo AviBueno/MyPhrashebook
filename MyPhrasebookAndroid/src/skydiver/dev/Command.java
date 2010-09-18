ae470bfb4fb80d907ebed40be87e56aec80b40d4
avi <avi>
2010-09-18 20:30:46 +0000

    Baseline version

---
+++ MyPhrasebookAndroid/src/skydiver/dev/Command.java
@@ -0,0 +1,8 @@
+package skydiver.dev;
+
+public interface Command
+{
+	public void execute();
+
+	public static final Command NO_OP = new Command() { public void execute() {} };
+}
---
+++ MyPhrasebookAndroid/src/skydiver/dev/DialogBuilder.java
@@ -0,0 +1,61 @@
+package skydiver.dev;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class DialogBuilder {
+	//private static final DialogCommandWrapper DISMISS_DLG_CMD = new DialogCommandWrapper(Command.NO_OP);
+	
+	public static AlertDialog.Builder buildMessageBox(
+			final Context context,
+			final String titleText,
+			final String bodyText,
+			final int okStringId,
+			final Command okCommand
+		)
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setCancelable(true);
+		builder.setTitle(titleText);
+		builder.setMessage(bodyText);
+		builder.setInverseBackgroundForced(true);
+		builder.setPositiveButton(context.getString(okStringId), new DialogCommandWrapper(okCommand));
+		return builder;
+	}
+
+	public static AlertDialog.Builder buildYesNoDialog(
+			final Context context,
+			final String titleText,
+			final String bodyText,
+			final int yesStringId,
+			final Command yesCommand,
+			final int noStringId,
+			final Command noCommand
+		)
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setCancelable(true);
+		builder.setTitle(titleText);
+		builder.setMessage(bodyText);
+		builder.setInverseBackgroundForced(true);
+		builder.setPositiveButton(context.getString(yesStringId), new DialogCommandWrapper(yesCommand));
+		builder.setNegativeButton(context.getString(noStringId), new DialogCommandWrapper(noCommand));
+		
+		return builder;
+	}
+
+	static final class DialogCommandWrapper implements DialogInterface.OnClickListener {
+		private Command command;
+
+		public DialogCommandWrapper(Command command) {
+			this.command = command;
+		}
+
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			dialog.dismiss();
+			command.execute();
+		}
+	}
+}
